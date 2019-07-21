4c292f806f75951fe0bf3de93424f3a76b82ba56
Erik <erikborella@gmail.com>
2019-07-20 21:41:58 -0300

    create room modal and jquerry script

---
+++ static/js/create-room.js
@@ -0,0 +1,16 @@
+$(document).ready(() => {
+    $('#jf_create_create').click(() => {
+        let room_name = $('#jf_create-room_name').val();
+
+        if (room_name) {
+            $.ajax({
+                type: 'POST',
+                url: 'create_room',
+                data: {name: room_name},
+                success: (data) => {
+                    console.log(data)
+                }
+            })
+        }
+    });
+});
